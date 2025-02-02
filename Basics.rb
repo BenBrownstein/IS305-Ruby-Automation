5db5716c25e668567a27eacd8288178af65cbd5c
BEN-PC\Shock887 <benb1of1@gmail.com>
2025-02-02 01:39:22 -0800

    Basic coding for Ruby to start learning the language

---
+++ Basics.rb
@@ -0,0 +1,25 @@
+num = 3 ** 2
+string = "Test Words"
+puts string
+puts string.length * num
+puts string.upcase
+puts string.downcase
+
+if num > 5
+    puts "Big Number"
+else
+    puts "Small Number"
+end
+
+i = 0
+while i < 5 do
+    puts i
+    i += 1
+end
+
+puts i
+
+until i < 0 do 
+    puts i
+    i = i -1
+end
