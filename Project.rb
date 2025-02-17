172d248aafbfd047415e06b76567f52c33d61ded
BEN-PC\Shock887 <benb1of1@gmail.com>
2025-02-17 15:50:45 -0800

    Got join function working and have two test files added it currently using absolute path for it to function

--- Project.rb
+++ Project.rb
@@ -1,15 +1,23 @@
 #PDF Multitool Project File
+require 'combine_pdf'
+
 
 #Functions
 =begin
 split(file, page_number)
   splits the pdf file at the page number
   return newfile1, newfile2
+=end
 
-join(file1, file2)
-  adds file2 to the end of file1
-  return newfile
+def join(file1, file2, filename = "combined.pdf")
+  pdf = CombinePDF.new
+  pdf << CombinePDF.load(file1) # Load and append the first PDF
+  pdf << CombinePDF.load(file2) # Load and append the second PDF
+  pdf.save(filename) # Save the merged PDF
+  puts "PDFs merged successfully into #{filename}!"
+end 
 
+=begin
 chunk (file, pagenumber1, pagenumber2)
   makes a series of pages a seperate file in  the range of pagenumber1 to pagenumber2
   return newfile
@@ -23,3 +31,4 @@ make_Form_Fillable(file, pagenumber, coordx, coordy, size)
   return newfile
 =end
 
+join("D:/IS 305 Ruby/python toolbox certificate.pdf", "D:/IS 305 Ruby/intro to importing data cert.pdf")
