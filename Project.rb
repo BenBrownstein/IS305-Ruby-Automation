653fa071bbf46a6472c9c96721456afb2a63fc95
BEN-PC\Shock887 <benb1of1@gmail.com>
2025-03-15 03:45:22 -0700

    Added file selection. Made automated detection of a pdf file going into make plain text folder where it runs convert to plain text on it and in theory puts it in out put. It is creating a ghost file

--- Project.rb
+++ Project.rb
@@ -1,7 +1,9 @@
 #PDF Multitool Project File
-require 'combine_pdf'
-require 'hexapdf'
-require 'pdf-reader'
+require "combine_pdf"
+require "hexapdf"
+require "pdf-reader"
+require "tk"
+require "listen"
 
 #Functions
 def split(file, page_number, filename1: "split_front.pdf", filename2: "split_back.pdf")
@@ -25,9 +27,9 @@ def join_pdf(file1, file2, filename: "combined.pdf")
   pdf << CombinePDF.load(file2) # Load and append the second PDF
   pdf.save(filename) # Save the merged PDF
   puts "PDFs merged successfully into #{filename}!"
-end 
+end
 
-def chunk (file, page_number1, page_number2, filename: "chunk.pdf")
+def chunk(file, page_number1, page_number2, filename: "chunk.pdf")
   pdf = CombinePDF.load(file)
   chunk_pdf = CombinePDF.new
   if page_number1 == page_number2
@@ -40,20 +42,7 @@ def chunk (file, page_number1, page_number2, filename: "chunk.pdf")
   chunk_pdf.save(filename)
 end
 
-
-def convert_To_Plain_Text(file, filename: "text_from_pdf.txt")
-reader = PDF::Reader.new(file)
-text = ""
-reader.pages.each do |page|
-  text += page.text
-end
-File.open(filename, "w") do |file|
-  file.write(text)
-end
-end
-
-
-def make_Form_Fillable(file, coordx, coordy, width, height, page_number: 1, fieldname:"text_field", filename: "form-fillable.pdf", single: true)
+def make_Form_Fillable(file, coordx, coordy, width, height, page_number: 1, fieldname: "text_field", filename: "form-fillable.pdf", single: true)
   doc = HexaPDF::Document.open(file)
   form = doc.acro_form(create: true)
   if single == true
@@ -61,15 +50,57 @@ def make_Form_Fillable(file, coordx, coordy, width, height, page_number: 1, fiel
   else
     field = form.create_multiline_text_field(fieldname)
   end
-  page = doc.pages[page_number-1]
+  page = doc.pages[page_number - 1]
   widget = field.create_widget(page, Rect: [coordx, coordy, coordx + width, coordy + height])
   page[:Annots] = [] unless page[:Annots]
-  page[:Annots] << widget  
+  page[:Annots] << widget
   doc.write(filename)
 end
 
+def file_selection()
+  filename = Tk.getOpenFile
+  if filename.end_with?(".pdf")
+    puts "This is a valid file"
+    return filename
+  else
+    raise ArgumentError, "This is not a .pdf file"
+  end
+end
 
-# join_pdf("D:/IS 305 Ruby/python toolbox certificate.pdf", "D:/IS 305 Ruby/intro to importing data cert.pdf")
+def convert_To_Plain_Text(file, filename: "text_from_pdf.txt")
+  reader = PDF::Reader.new(file)
+  text = ""
+  reader.pages.each do |page|
+    text += page.text
+  end
+  
+  File.open(filename, "w") do |file|
+    file.write(text)
+  end
+end
+
+def process_new_file_to_text(file)
+  # output_dir = "D:/IS 305 Ruby/Output"
+  # filename = File.join(output_dir, "Plain_Text_" + File.basename(file) + ".txt")
+  puts "New file detected: #{file}"
+  convert_To_Plain_Text(file)
+end
+
+watch_dir_plain_text = "D:/IS 305 Ruby/Make Plain Text"
+listener1 = Listen.to(watch_dir_plain_text, only: /\.pdf$/) do |modified, added, removed|
+  added.each do |file|
+    process_new_file_to_text(file) if File.file?(file)  # Check if the added item is a file
+  end
+end
+
+puts "Watching folder for new PDF files..."
+listener1.start
+sleep
+
+
+# file1 = file_selection()
+# file2 = file_selection()
+# join_pdf(file1, file2)
 
 # split("D:/IS 305 Ruby/Ben Brownstein IS305 Project Report.pdf", 3)
 
